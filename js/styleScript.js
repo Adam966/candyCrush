9f7108e823ee4620b45e4b6e4590bcb6c73bc47d
tomvarga620 <tomas.varga@akademiasovy.sk>
2018-12-08 22:09:10 +0100

    pridany fade effect pre modal - Rules a Score

--- js/styleScript.js
+++ js/styleScript.js
@@ -4,39 +4,43 @@ function modal(){
 
 // Get the modal
 let modalRules = document.getElementById('rulesModal');
+let modalScore = document.getElementById('scoreModal');
 
 // Get the button that opens the modal
 let btnRules = document.getElementById("rulesButton");
+let btnScore = document.getElementById("scoreButton");
 
 // Get the <span> element that closes the modal
 let span = document.getElementsByClassName("close")[0];
 
 let span2 = document.getElementsByClassName('close2')[0];
 
-let btnScore = document.getElementById('scoreButton');
-
 // When the user clicks the button, open the modal
 btnRules.onclick = function() {
-    modalRules.style.display = "block";
+    $(modalRules).fadeIn("fast");
+}
+
+btnScore.onclick = function() {
+  	$(modalScore).fadeIn("fast");
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modalRules.style.display = "none";
-}
-
-btnScore.onclick = function() {
-  scoreModal.style.display = "block";
+    $(modalRules).fadeOut("fast");
 }
 
 span2.onclick = function() {
-  scoreModal.style.display = "none";
+  	$(modalScore).fadeOut("fast");
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modalRules) {
-        modalRules.style.display = "none";
+        $(modalRules).fadeOut("fast");
+    }
+	
+	if (event.target == modalScore) {
+        $(modalScore).fadeOut("fast");
     }
 
 }
